40defb7cbaafd3508ae5e88711311a7ec4a658bd
kevin young <you.k2208@gmail.com>
2024-08-07 16:49:19 +0900

    completed index and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning the DB..."
+Restaurant.destroy_all
+
+5.times do
+  Restaurant.create(
+    name:  Faker::Restaurant.name,
+    address: Faker::Address.city,
+    category: Restaurant::CATEGORIES.sample
+  )
+end
+
+puts "... created #{Restaurant.count} restaurants"
